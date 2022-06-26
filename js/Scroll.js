b0ef593bf9338a7b94672bf0434793eaa0098675
Caio Oliveira <106616005+CaiOliveira19@users.noreply.github.com>
2022-06-25 21:22:46 -0300

    Update

---
+++ js/Scroll.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 400){
+            $(".header").css({"opacity" : "1"})
+        }
+        else{
+            $(".header").css({"opacity" : "0"})
+        }
+    })
+})
+
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 400){
+            $(".header").css({"z-index" : "99",})
+        }
+        else{
+            $(".header").css({"z-index" : "-1"})
+        }
+    })
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 250){
+            $(".txt_info").css({"opacity" : "0"})
+        }
+        else{
+            $(".txt_info").css({"opacity" : "1"})
+        }
+    })
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 400){
+            $(".one").css({"opacity" : "0"})
+        }
+        else{
+            $(".one").css({"opacity" : "1"})
+        }
+    })
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 650){
+            $(".two").css({"opacity" : "0"})
+        }
+        else{
+            $(".two").css({"opacity" : "1"})
+        }
+    })
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 950){
+            $(".three").css({"opacity" : "0"})
+        }
+        else{
+            $(".three").css({"opacity" : "1"})
+        }
+    })
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 1200){
+            $(".four").css({"opacity" : "0"})
+        }
+        else{
+            $(".four").css({"opacity" : "1"})
+        }
+    })
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 1450){
+            $(".five").css({"opacity" : "0"})
+        }
+        else{
+            $(".five").css({"opacity" : "1"})
+        }
+    })
+})
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 1800){
+            $("#Contato").css({"opacity" : "0"})
+        }
+        else{
+            $("#Contato").css({"opacity" : "1"})
+        }
+    })
+})
+
+
---
+++ js/TweenMax.js
@@ -0,0 +1,106 @@
+TweenMax.to(".Preload h1", 2, {
+    opacity: 0,
+    y: -60,
+    ease: Expo.easeInOut
+  })
+
+TweenMax.to(".Preload span", 2, {
+    delay: .3,
+    opacity: 0,
+    y: -60,
+    ease: Expo.easeInOut
+  })
+
+TweenMax.to(".Preload", 2, {
+    delay: 1,
+    top: "-100%",
+    ease: Expo.easeInOut
+  })
+
+  TweenMax.from(".wolf_img", 1, {
+    delay: 1.6,
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+
+TweenMax.from(".ellipse-container", 1, {
+    delay: 2,
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+
+TweenMax.from(".yellow", 1, {
+    delay: 3.5,
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+
+TweenMax.from(".circle1", 1, {
+    delay: 2.4,
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+
+TweenMax.from(".circle2", 1, {
+    delay: 2.6,
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+
+TweenMax.from(".logo", 1, {
+    delay: 3,
+    opacity: 0,
+    y: -100,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from("header", 1, {
+    delay: 3,
+    opacity: 0,
+    y: -100,
+      ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".Menu ul li", 1, {
+    delay: 3.2,
+    opacity: 0,
+    x: -100,
+    ease: Expo.easeInOut
+}, 0.08)
+
+TweenMax.from(".scrolldown", 1, {
+    delay: 3.4,
+    opacity: 0,
+    y: 100,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".txt_Wolfs .title", 1, {
+    delay: 3,
+    opacity: 0,
+    x: 200,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".txt_Wolfs p", 1, {
+    delay: 3.2,
+    opacity: 0,
+    x: 200,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".About", 1, {
+    delay: 3.4,
+    opacity: 0,
+    x: 200,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".media ul li", 1, {
+    delay: 3,
+    opacity: 0,
+    y: 100,
+    ease: Expo.easeInOut
+},0.08)
+
+ 
